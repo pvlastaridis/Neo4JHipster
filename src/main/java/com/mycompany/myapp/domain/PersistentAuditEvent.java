885aaa5b962de05b30ea0143f77ce6cafaf36dd3
pvlastaridis <panosvlastaridis@gmail.com>
2014-10-29 04:02:52 +0200

    Forgot Some Files

---
+++ src/main/java/com/mycompany/myapp/domain/PersistentAuditEvent.java
@@ -0,0 +1,29 @@
+package com.mycompany.myapp.domain;
+
+import java.util.Map;
+
+import org.joda.time.LocalDateTime;
+
+public class PersistentAuditEvent {
+
+	public LocalDateTime getAuditEventDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAuditEventType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<String, String> getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPrincipal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
